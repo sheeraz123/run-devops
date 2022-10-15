8db0ddf85d41746c9a73eeee1b5d9f79d38e1bcf
sheeraz123 <ahmed.sheeraz1@gmail.com>
2022-10-16 00:43:28 +0530

    First Commit

---
+++ shopping/shopping.client/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace shopping.client.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ shopping/shopping.client/Models/Product.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace shopping.client.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public string Description { get; set; }
+        public string ImageFile { get; set; }
+        public decimal Price { get; set; }
+
+    }
+}
